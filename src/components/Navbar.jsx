cd3469109edd10c2c89b315f7ad9f04bad6fe50b
pcatalinahe <hperezcatalina@gmail.com>
2025-03-18 20:11:21 -0400

    hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import "../../public/css/Navbar.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav>
-      <Link to="/" className="logo">
+      <NavLink to="/" className="logo">
         <img src="../../images/logo.png" className="logo-img"></img>
-      </Link>
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/team">Leadership</Link>
-        <Link to="/plants">Plants</Link>
-        <button className="nav-login-button">
-          <Link to="/login">Login/Sign Up</Link>
-        </button>
+      </NavLink>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
+      <ul className={menuOpen ? "open" : ""} onClick={() => setMenuOpen(false)}>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/team">Leadership</NavLink>
+        </li>
+        <li>
+          <NavLink to="/plants">Plants</NavLink>
+        </li>
+        <li>
+          <button className="nav-login-button">
+            <NavLink to="/login">Login/Sign Up</NavLink>
+          </button>
+        </li>
+      </ul>
     </nav>
   );
 };
