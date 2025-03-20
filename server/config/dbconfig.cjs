baec4a69ab80ecf794d1e02ae8d7943b3cf44ab0
Khalia <kkhhaalliiaa011@gmail.com>
2025-03-19 21:08:14 -0400

    fixed database

--- server/config/dbconfig.cjs
+++ server/config/dbconfig.cjs
@@ -3,11 +3,19 @@ const mysql = require('mysql2');
 
 console.log('Connecting to DB:', process.env.DB_HOST, process.env.DB_USER, process.env.DB_NAME);
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+
+connection.getConnection((error, connection) => {
+
+    if(error){
+      process.exit(1);
+    };
+    connection.release();
+  });
 
 module.exports = connection;
